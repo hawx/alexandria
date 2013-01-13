81c20c43e39a46b0290a17548c1a97ac8b663583
Joshua Hawxwell <m@hawx.me>
2013-01-13 19:20:16 +0000

    Now store books in folders with name also

--- lib/alexandria/library.rb
+++ lib/alexandria/library.rb
@@ -2,14 +2,6 @@
 require 'highline/import'
 
 module Alexandria
-
-  
-  # .
-  # |- author1
-  # |    |- book1
-  # |    |- book2
-  # |- author2
-  #      |- book1    
   
   class Library
   
@@ -21,21 +13,24 @@ module Alexandria
     
     # Physical
     def add!(book_path)
+      book_path = File.expand_path(book_path)
+    
       # Get title and author
       temp = Book.create(book_path)
       
-      dest = @path + temp.author + (temp.title + temp.extension)
+      dir  = @path + temp.author + temp.title
+      dest = dir + (temp.title + temp.extension)
       
       # If it exists, ask to replace
       if dest.exist?
-        return unless ask(" Book already exists!".red + " Replace?  ")
+        return unless ask(" Book already exists!".red + " Replace? [y/n] ")
       end
       
       puts " moving".grey + " #{book_path}"
       puts "     to".grey + " #{dest}"
       
       # Create dirs, and move
-      FileUtils.mkdir_p @path + temp.author
+      FileUtils.mkdir_p dir
       FileUtils.cp book_path, dest
     end
     
