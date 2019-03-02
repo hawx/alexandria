f115a50c2c691dce11ba74aa04349eddb3720801
Joshua Hawxwell <m@hawx.me>
2019-03-02 14:49:38 +0000

    Use indieauth/sessions package

--- alexandria.go
+++ alexandria.go
@@ -1,21 +1,18 @@
 package main
 
 import (
-	"crypto/rand"
-	"encoding/base64"
 	"flag"
-	"io"
 	"log"
 	"net/http"
 
 	"github.com/BurntSushi/toml"
-	"github.com/gorilla/sessions"
 	"hawx.me/code/alexandria/data"
 	"hawx.me/code/alexandria/web/assets"
 	"hawx.me/code/alexandria/web/events"
 	"hawx.me/code/alexandria/web/filters"
 	"hawx.me/code/alexandria/web/handlers"
 	"hawx.me/code/indieauth"
+	"hawx.me/code/indieauth/sessions"
 	"hawx.me/code/mux"
 	"hawx.me/code/route"
 	"hawx.me/code/serve"
@@ -41,16 +38,12 @@ func main() {
 		log.Fatal("toml:", err)
 	}
 
-	if conf.Me == "" {
-		log.Fatal("me must be set to sign-in")
-	}
-
-	auth, err := indieauth.Authentication(conf.URL, conf.URL+"/callback")
+	auth, err := indieauth.Authentication(conf.URL, conf.URL+"callback")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	endpoints, err := indieauth.FindEndpoints(conf.Me)
+	session, err := sessions.New(conf.Me, conf.Secret, auth)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -61,66 +54,15 @@ func main() {
 	es := events.New()
 	defer es.Close()
 
-	store := NewStore(conf.Secret)
+	route.Handle("/", mux.Method{"GET": session.Choose(handlers.List(true), handlers.List(false))})
+	route.Handle("/books", session.Shield(handlers.AllBooks(db, es)))
+	route.Handle("/books/:id", session.Shield(handlers.Books(db, es)))
+	route.Handle("/editions/:id", session.Shield(handlers.Editions(db, conf.BooksPath)))
+	route.Handle("/upload", session.Shield(handlers.Upload(db, es, conf.BooksPath)))
 
-	protect := func(good, bad http.Handler) http.Handler {
-		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			if addr := store.Get(r); addr == conf.Me {
-				good.ServeHTTP(w, r)
-			} else {
-				bad.ServeHTTP(w, r)
-			}
-		})
-	}
-
-	shield := func(h http.Handler) http.Handler {
-		return protect(h, http.NotFoundHandler())
-	}
-
-	route.Handle("/", mux.Method{"GET": protect(handlers.List(true), handlers.List(false))})
-	route.Handle("/books", shield(handlers.AllBooks(db, es)))
-	route.Handle("/books/:id", shield(handlers.Books(db, es)))
-	route.Handle("/editions/:id", shield(handlers.Editions(db, conf.BooksPath)))
-	route.Handle("/upload", shield(handlers.Upload(db, es, conf.BooksPath)))
-
-	route.HandleFunc("/sign-in", func(w http.ResponseWriter, r *http.Request) {
-		state, err := store.SetState(w, r)
-		if err != nil {
-			http.Error(w, "could not start auth", http.StatusInternalServerError)
-			return
-		}
-
-		redirectURL := auth.RedirectURL(endpoints, conf.Me, state)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		http.Redirect(w, r, redirectURL, http.StatusFound)
-	})
-
-	route.HandleFunc("/callback", func(w http.ResponseWriter, r *http.Request) {
-		state := store.GetState(r)
-
-		if r.FormValue("state") != state {
-			http.Error(w, "state is bad", http.StatusBadRequest)
-			return
-		}
-
-		me, err := auth.Exchange(endpoints, r.FormValue("code"))
-		if err != nil || me != conf.Me {
-			http.Error(w, "nope", http.StatusForbidden)
-			return
-		}
-
-		store.Set(w, r, me)
-		http.Redirect(w, r, "/", http.StatusFound)
-	})
-
-	route.HandleFunc("/sign-out", func(w http.ResponseWriter, r *http.Request) {
-		store.Set(w, r, "")
-		http.Redirect(w, r, "/", http.StatusFound)
-	})
+	route.Handle("/sign-in", session.SignIn())
+	route.Handle("/callback", session.Callback())
+	route.Handle("/sign-out", session.SignOut())
 
 	route.Handle("/events", es)
 	route.Handle("/assets/*filepath", http.StripPrefix("/assets/", assets.Server(map[string]string{
@@ -132,52 +74,4 @@ func main() {
 	})))
 
 	serve.Serve(*port, *socket, filters.Log(route.Default))
-}
-
-type meStore struct {
-	store sessions.Store
-}
-
-func NewStore(secret string) *meStore {
-	return &meStore{sessions.NewCookieStore([]byte(secret))}
-}
-
-func (s meStore) Get(r *http.Request) string {
-	session, _ := s.store.Get(r, "session")
-
-	if v, ok := session.Values["me"].(string); ok {
-		return v
-	}
-
-	return ""
-}
-
-func (s meStore) Set(w http.ResponseWriter, r *http.Request, me string) {
-	session, _ := s.store.Get(r, "session")
-	session.Values["me"] = me
-	session.Save(r, w)
-}
-
-func (s meStore) SetState(w http.ResponseWriter, r *http.Request) (string, error) {
-	bytes := make([]byte, 32)
-
-	if _, err := io.ReadFull(rand.Reader, bytes); err != nil {
-		return "", err
-	}
-
-	state := base64.StdEncoding.EncodeToString(bytes)
-
-	session, _ := s.store.Get(r, "session")
-	session.Values["state"] = state
-	return state, session.Save(r, w)
-}
-
-func (s meStore) GetState(r *http.Request) string {
-	session, _ := s.store.Get(r, "session")
-
-	if v, ok := session.Values["state"].(string); ok {
-		return v
-	}
-
-	return ""
 }
--- go.mod
+++ go.mod
@@ -6,7 +6,7 @@ require (
 	github.com/boltdb/bolt v1.3.1
 	github.com/google/uuid v1.1.1
 	github.com/gorilla/sessions v1.1.3 // indirect
-	hawx.me/code/indieauth v0.0.0-20190217173404-03d7e4c53487 // indirect
+	hawx.me/code/indieauth v0.0.0-20190302144758-4bab90bdd555 // indirect
 	hawx.me/code/mux v0.0.0-20171229202905-76b5c781b7be
 	hawx.me/code/route v1.0.0
 	hawx.me/code/serve v0.0.0-20190207181551-eb94630184cf // indirect
